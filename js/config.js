4060a2775e3742c146e898fcb39ffc5a25133fd1
UserCC2XX <greet.mohamed@gmail.com>
2024-11-20 10:10:34 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_nplyJX7TB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4068ie0agc4gucjd66kageumlf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
